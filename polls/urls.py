525762979471b591547d3eb01db7ec5464286b4c
Magi <magi@magi.com>
2018-11-13 22:16:53 -0200

    Fixed urls

--- polls/urls.py
+++ polls/urls.py
@@ -4,25 +4,25 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.IndexView.as_view(), name='index'),
+    path('polls/', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
     # ex: /polls/create/
-    path('create/', views.createPage, name='create_page'),
+    path('polls/create/', views.createPage, name='create_page'),
     # ex: /polls/create/object/
-    path('create/object', views.create, name='create'),
+    path('polls/create/object', views.create, name='create'),
     # ex: /polls/create/choice/
-    path('create/choice', views.CreateChoicePage.as_view(), name='create_choice_page'),
+    path('polls/create/choice', views.CreateChoicePage.as_view(), name='create_choice_page'),
     # ex: /polls/What's going on/create
-    path('delete/<int:pk>/', views.deleteView.as_view(), name='delete_question'),
+    path('polls/delete/<int:pk>/', views.deleteView.as_view(), name='delete_question'),
     # ex: polls/delete/32
 
-    path('create/choice/<int:pk>', views.create_choice, name='create_choice'),
+    path('polls/create/choice/<int:pk>', views.create_choice, name='create_choice'),
 
-    path('gg', views.newIndex, name='dashboard_index'),
+    path('', views.newIndex, name='dashboard_index'),
 
 ]
